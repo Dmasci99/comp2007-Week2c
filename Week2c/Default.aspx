a27eac38ee9e7f46e76abf8176864e6e5d264968
Dan Masci <minimasci@yahoo.ca>
2016-05-19 21:14:57 -0400

    Project Base Created

---
+++ Week2c/Default.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="HomePage" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Week2c.Default" %>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <h1>Home Page</h1>
+    </div>
+</asp:Content>
---
+++ Week2c/Site.Master.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Week2c
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ChangeActivePageLink();
+        }
+
+        protected void ChangeActivePageLink()
+        {
+            switch (Page.Title)
+            {
+                case "HomePage": HomePage.Attributes.Add("class", "active");
+                    break;
+                case "AboutPage":
+                    HomePage.Attributes.Add("class", "active");
+                    break;
+                case "ProductsPage":
+                    HomePage.Attributes.Add("class", "active");
+                    break;
+                case "ServicesPage":
+                    HomePage.Attributes.Add("class", "active");
+                    break;
+                case "ContactPage":
+                    HomePage.Attributes.Add("class", "active");
+                    break;
+            }
+        }
+    }
+}
